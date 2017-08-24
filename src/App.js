5e864b5f618ffd8ef28284c5848ae8cc8acbc534
BRENDA <brendagutierrez@iMac-de-Brenda.local>
2017-08-24 16:38:00 -0500

    consumo de api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor(){
+          super();
+    this.state = {apiResponse:""}
+  }
+
+  componentWillMount(){
+    fetch("https://itunes.apple.com/search?term=taylor+swift")
+    .then((response) => { return(response.json())})
+    .then((objetoJSON) =>{
+		console.log(objetoJSON)
+        return(objetoJSON)
+//      this.setState({apiResponse:objetoJSON})
+    })
+  
+  }
+    
+    
+    
+    render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
